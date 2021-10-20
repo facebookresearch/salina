76f6996158229c10d13ef2a8ba6d3f315dd635d6
Ludovic Denoyer <denoyer@fb.com>
2021-10-20 16:45:16 +0200

    ...

--- salina_examples/rl/ppo_brax/ppo.py
+++ salina_examples/rl/ppo_brax/ppo.py
@@ -36,12 +36,12 @@ class BatchNormalizer(Agent):
 
     def forward(self, t, update_normalizer=True, **args):
         print(self.training,self.bn.training)
-        if update_normalizer:
-            print("ici")
-            self.bn.train()
-        else:
-            print("ici eval")
-            self.bn.eval()
+        # if update_normalizer:
+        #     print("ici")
+        #     self.bn.train()
+        # else:
+        #     print("ici eval")
+        #     self.bn.eval()
         print("-- ",self.training,self.bn.training)
         input = self.get(("env/env_obs", t))
         print("coucou ",input.size())
