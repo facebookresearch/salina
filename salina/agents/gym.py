df8b60754587e03368c861bcb6c07feb6641a1af
Jean-Baptiste Gaya <jbgaya@devfair0267.h2.fair>
2022-02-02 02:08:13 -0800

    fixing headers

--- salina/agents/gym.py
+++ salina/agents/gym.py
@@ -1,3 +1,9 @@
+#
+# Copyright (c) Facebook, Inc. and its affiliates.
+#
+# This source code is licensed under the MIT license found in the
+# LICENSE file in the root directory of this source tree.
+#
 print(
     "[***** BUG FIX *******] Use import salina.agents.gyma instead of import salina.agents.gym"
 )
